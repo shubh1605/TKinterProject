2c11b1538d92f7468e23b825f33db2ae5c5a0217
shubh0208 <62873923+shubh0208@users.noreply.github.com>
2021-04-14 17:55:26 +0530

    Search added

--- Hisaab.py
+++ Hisaab.py
@@ -2,6 +2,7 @@ from tkinter import *
 import sqlite3
 from tkcalendar import DateEntry
 import datetime
+from tkinter import ttk
 root =Tk()
 
 # conn = sqlite3.connect('hisaab.db')
@@ -20,19 +21,22 @@ root =Tk()
 # 			amount real,
 # 			current_balance real,
 # 			date_of_entry text,
-# 			Reason text null
+# 			Reason text null,
+# 			Category text 
 # 			) """)
 
 # conn.commit()
 # conn.close()
 
 type_ = StringVar()
+category_ = StringVar()
 
-type_label = Label(root, text= "Type: ").grid(row=0, column =0)
+type_label = Label(root, text= "Type:").grid(row=0, column =0)
 opposite_party_label = Label(root, text= "opposite Party: ").grid(row=1, column =0) 
 date_of_transaction_label = Label(root, text= "date of transaction: ").grid(row=2, column =0) 
 amount_label = Label(root, text= "Amount : ").grid(row=3, column =0) 
 reason_label = Label(root, text= "Reason : ").grid(row=4, column =0)
+category_label = Label(root, text= "Category: ").grid(row=5, column =0)
 
 #type_ = Entry(root,  width = 35, borderwidth = 5)
 type_debit = Radiobutton(root, text="Debit", value="debit", variable=type_)
@@ -42,6 +46,8 @@ opposite_party = Entry(root, width = 35, borderwidth = 5)
 date_of_transaction = DateEntry(root, width = 35, borderwidth = 5)
 amount = Entry(root, width = 35, borderwidth = 5)
 reason = Text(root, width = 35, borderwidth = 5, height = 3)
+category = ttk.Combobox(root, width= 35, textvariable= category_)
+category["values"] = ("Food","Home expense","Loan","Work expense","Miscellaneous","-")
 
 type_credit.grid(row=0, column=1)
 type_debit.grid(row=0, column=2)
@@ -49,6 +55,7 @@ opposite_party.grid(row=1,column=1)
 date_of_transaction.grid(row=2,column=1)
 amount.grid(row=3,column=1)
 reason.grid(row=4,column=1)
+category.grid(row=5, column =1)
 
 opposite_party.insert(0,"Enter the opposite party name")
 amount.insert(0,"Enter the amount of transaction")
@@ -75,7 +82,7 @@ def submit():
 	date = datetime.date.today()
 
 
-	c.execute(" INSERT INTO records VALUES (:type, :opposite_party, :date_of_transaction, :amount, :current_balance,:date_of_entry, :reason) ",
+	c.execute(" INSERT INTO records VALUES (:type, :opposite_party, :date_of_transaction, :amount, :current_balance,:date_of_entry, :reason, :category) ",
 				{
 					"type": type_.get(),
 					"opposite_party": opposite_party.get(),
@@ -83,11 +90,51 @@ def submit():
 					"date_of_entry":date.strftime("%m/%d/%y"),   
 					"amount": amount.get(),
 					"current_balance": cur_balance,
-					"reason": reason.get("1.0",END)
+					"reason": reason.get("1.0",END),
+					"category": category_.get()
 				})
 
 	conn.commit()
 	conn.close()
+# OR type = search_key OR reason = search_key OR category = search_key
+def search_func(search_key):
+	print("Hiiiii")
+	conn = sqlite3.connect('hisaab.db')
+	c= conn.cursor()
+	
+	records.destroy()
+
+	query = str(search_key.get())
+	print("abc:",query)
+
+
+	c.execute("SELECT * FROM records WHERE opposite_party like ? or type like ? or category like ? or date_of_transaction like ?", (query,query,query,query,))
+	records_data = c.fetchall()
+	print(records_data)
+
+	global searched_records
+	searched_records = Toplevel()
+
+	type_head = Label(searched_records, text="Type").grid(row = 1, column =0)
+	party_head = Label(searched_records, text="party").grid(row = 1, column =1)
+	dateTransaction_head = Label(searched_records, text="Date of Transaction").grid(row = 1, column =2)	
+	amount_head = Label(searched_records, text="Amount").grid(row = 1, column =3)
+	currentBalance_head = Label(searched_records, text="Current balance").grid(row = 1, column =4)
+	dateEntry_head = Label(searched_records, text="Date of Entry").grid(row = 1, column =5)
+	reason_head = Label(searched_records, text="Reason").grid(row = 1, column =6, columnspan= 1)
+	category_head = Label(searched_records, text="Category").grid(row = 1, column =7, columnspan= 1)
+
+	for i in range(0, len(records_data)):
+		for j in range(8):
+			Label(searched_records, text=records_data[i][j]).grid(row= i+2,column = j)
+
+
+	back_button = Button(searched_records, text = "Back", command = lambda: [show_record(), searched_records.destroy()]).grid(row = 10,  column = 0)
+	home_button = Button(searched_records, text = "Home", command = lambda: [searched_records.destroy()]).grid(row = 10,  column = 1)
+
+	conn.commit()
+	conn.close()
+
 
 
 def show_record():
@@ -96,21 +143,39 @@ def show_record():
  	
 	c.execute("SELECT * FROM records")
 	records_data = c.fetchall()
-	
+
+	# if(searched_records != null):
+	# 	searched_records.destroy()
+
+	global records
 	records = Toplevel()
 
-	type_head = Label(records, text="Type").grid(row = 0, column =0)
-	party_head = Label(records, text="party").grid(row = 0, column =1)
-	dateTransaction_head = Label(records, text="Date of Transaction").grid(row = 0, column =2)	
-	amount_head = Label(records, text="Amount").grid(row = 0, column =3)
-	currentBalance_head = Label(records, text="Current balance").grid(row = 0, column =4)
-	dateEntry_head = Label(records, text="Date of Entry").grid(row = 0, column =5)
-	reason_head = Label(records, text="Reason").grid(row = 0, column =6, columnspan= 3)
+	global search
+	search_key = StringVar()
+	search = Entry(records, width = 35, borderwidth = 5, textvariable = search_key)
+	print(search_key.get())
+	search.insert(0, "Search here")
+	search.grid(row=0, column=0)
+	
 
+	search_button = Button(records, text = "Search", command = lambda: search_func(search_key))
+	search_button.grid(row =0, column = 1)
 
+	type_head = Label(records, text="Type").grid(row = 1, column =0)
+	party_head = Label(records, text="party").grid(row = 1, column =1)
+	dateTransaction_head = Label(records, text="Date of Transaction").grid(row = 1, column =2)	
+	amount_head = Label(records, text="Amount").grid(row = 1, column =3)
+	currentBalance_head = Label(records, text="Current balance").grid(row = 1, column =4)
+	dateEntry_head = Label(records, text="Date of Entry").grid(row = 1, column =5)
+	reason_head = Label(records, text="Reason").grid(row = 1, column =6, columnspan= 1)
+	category_head = Label(records, text="Category").grid(row = 1, column =7, columnspan= 1)
+
+	
 	for i in range(0, len(records_data)):
-		for j in range(7):
-			Label(records, text=records_data[i][j]).grid(row= i+1,column = j)
+		for j in range(8):
+			Label(records, text=records_data[i][j]).grid(row= i+2,column = j)
+
+			
 
 
 
